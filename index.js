95a38ca7e116092c7a9219f32fbfc2e6278b82cf
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-09-13 21:57:30 -0400

    iterate list, and keep track of accumulator. If the next value makes you net negative start over, otherwise take the larger of accumulator and greatest found.

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 export default (sequence) => {
+    let max = 0;
+    let running = 0;
+    let maxSeq = [];
+    let runningSeq = [];
+
+    sequence.forEach(element => {
+        running += element;
+        runningSeq.push(element);
+        if(running < 0) {
+            running = 0;
+            runningSeq = [];
+        }
+        else if(max < running) {
+            max = running;
+            maxSeq = runningSeq.slice(0);
+        }
+    });
+
     return {
-        max: 1,
-        maxSequence: [1]
+        max: max,
+        maxSequence: maxSeq
     };
-}
+};
